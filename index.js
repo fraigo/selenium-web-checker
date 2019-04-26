f191c026180b4f094557294753fedd0b9f1a8801
Francisco <franciscoigor@gmail.com>
2019-04-25 17:09:07 -0700

    Updated for node command line install

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
+#!/usr/bin/env node
+// Delete the 0 and 1 argument (node and script.js)
+var args = process.argv.splice(process.execArgv.length + 2);
+
+var fs = require('fs');
+var list = [];
+var file = args[0];
+if (!file){
+    console.error("Usage: selenium-web-checker [url|config.json]");
+    return;
+}
+
 var wd = require('webdriver-sync');
 var driver = require('./driver.js');
 var analyze = require('./analyze.js');
-console.log(analyze);
+function analyze_list(list,baseURL){
+    for(var index in list){
+        var item=list[index];
+        var url = item.url;
+        if (baseURL){
+            url = baseURL + url;
+        }
+        analyze(url);
+        if (item.submit){
+            for(var key in item.submit){
+                driver.findElement(wd.By.name(key)).sendKeys(item.submit[key]);
+            }    
+        }
+        if (item.click){
+            driver.findElement(wd.By.name(item.click)).click();    
+        }    
+    }    
+    driver.quit();
+}
 
-analyze("http://192.168.99.100:8000/user/login");
-driver.findElement(wd.By.name('email')).sendKeys('user@site.com');
-driver.findElement(wd.By.name('password')).sendKeys('User.123');
-driver.findElement(wd.By.name('submit')).click();
-analyze("http://192.168.99.100:8000/");
-driver.quit();
+
+if (fs.existsSync(file)){
+    //console.log("Opening "+ file);
+    fs.readFile(file, 'utf8', function(err, data) {  
+        var baseURL = null;
+        if (err) {
+            
+        }else{
+            list = JSON.parse(data).pages;
+            baseURL = JSON.parse(data).baseURL;
+            analyze_list(list,baseURL);
+        }
+
+    });
+}else{
+    analyze_list([{url:args[0]}],args[1]);
+}
